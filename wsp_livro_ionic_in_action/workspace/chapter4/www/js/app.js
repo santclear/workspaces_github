fc5e57f9510beecad9876c4a070252453e27a47d
Sant'Clear Ali Costa <santclear@gmail.com>
2016-04-19 20:04:54 -0300

    Capítulo 4 - Página 69

---
+++ wsp_livro_ionic_in_action/workspace/chapter4/www/js/app.js
@@ -0,0 +1,24 @@
+angular.module('App', ['ionic'])
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'views/home/home.html'
+    });
+  
+  $urlRouterProvider.otherwise('/home');
+
+})
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
