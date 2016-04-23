130ebcd43ff980032c3b38e1e21bf045d0e7a842
Sant'Clear Ali Costa <santclear@gmail.com>
2016-04-23 20:39:34 -0300

    Capítulo 5 - Página 98 - Step 2
    Implementação de tabulação.

--- wsp_livro_ionic_in_action/workspace/chapter5/www/js/app.js
+++ wsp_livro_ionic_in_action/workspace/chapter5/www/js/app.js
@@ -1,12 +1,23 @@
 angular.module('App', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+    .config(function ($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('tabs', {
+                url: '/tabs',
+                templateUrl: 'views/tabs/tabs.html'
+            });
+
+        $urlRouterProvider.otherwise('/tabs');
+    })
+
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    });
