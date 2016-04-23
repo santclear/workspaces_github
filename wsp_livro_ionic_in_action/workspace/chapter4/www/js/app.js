0bb40db8070c35b2bdb348eb5b1c40441532684e
Sant'Clear Ali Costa <santclear@gmail.com>
2016-04-23 17:17:58 -0300

    Concluído capítulo 4 - Step 7

--- wsp_livro_ionic_in_action/workspace/chapter4/www/js/app.js
+++ wsp_livro_ionic_in_action/workspace/chapter4/www/js/app.js
@@ -1,39 +1,43 @@
 angular.module('App', ['ionic'])
 
-.config(function ($stateProvider, $urlRouterProvider) {
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: 'views/home/home.html'
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: 'views/home/home.html'
+            })
+            .state('reservation', {
+                url: '/reservation',
+                controller: 'ReservationController',
+                templateUrl: 'views/reservation/reservation.html'
+            })
+            .state('weather', {
+                url: '/weather',
+                controller: 'WeatherController',
+                templateUrl: 'views/weather/weather.html'
+            })
+            .state('restaurants', {
+                url: '/restaurants',
+                controller: 'RestaurantsController',
+                templateUrl: 'views/restaurants/restaurants.html'
+            })
+            .state('tour', {
+                url: '/tour',
+                templateUrl: 'views/tour/tour.html'
+            });
+
+        $urlRouterProvider.otherwise('/tour');
+
     })
-    .state('reservation', {
-      url: '/reservation',
-      controller: 'ReservationController',
-      templateUrl: 'views/reservation/reservation.html'
+
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
     })
-    .state('weather', {
-      url: '/weather',
-      controller: 'WeatherController',
-      templateUrl: 'views/weather/weather.html'
-    })
-    .state('restaurants', {
-      url: '/restaurants',
-      controller: 'RestaurantsController',
-      templateUrl: 'views/restaurants/restaurants.html'
-    });
-
-  $urlRouterProvider.otherwise('/home');
-
-})
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
--- wsp_livro_ionic_in_action/workspace/chapter4/www/views/restaurants/restaurants.js
+++ wsp_livro_ionic_in_action/workspace/chapter4/www/views/restaurants/restaurants.js
@@ -1,28 +1,24 @@
 angular.module('App')
-.controller('RestaurantsController', function ($scope, $http) {
+    .controller('RestaurantsController', function ($scope, $http) {
 
-  $scope.page = 0;
-  $scope.total = 1;
-  $scope.restaurants = [];
+        $scope.page = 0;
+        $scope.total = 1;
+        $scope.restaurants = [];
 
-  $scope.getRestaurants = function () {
-    // Incrementa valor de página e faz uma solicitação HTTP para os dados
-    $scope.page++;
-    $http.get('https://ionic-in-action-api.herokuapp.com/restaurants?page=' + $scope.page).success(function (response) {
-      // Pega a lista de restaurantes e os adiciona à matriz restaurants[] para o ngRepeat
-      angular.forEach(response.restaurants, function (restaurant) {
-        $scope.restaurants.push(restaurant);
-      });
-      // Atualiza o total de páginas com base no valor enviado pelo API
-      $scope.total = response.totalPages;
-      // Evento Broadcast avisará quando o componente Infinite Scroll tiver terminado tudo
-      $scope.$broadcast('scroll.infiniteScrollComplete');
-    }).error(function (err) {
-      $scope.$broadcast('scroll.infiniteScrollComplete');
-      console.log(err);
+        $scope.getRestaurants = function () {
+            // Incrementa valor de página e faz uma solicitação HTTP para os dados
+            $scope.page++;
+            $http.get('https://ionic-in-action-api.herokuapp.com/restaurants?page=' + $scope.page).success(function (response) {
+                angular.forEach(response.restaurants, function (restaurant) {
+                    $scope.restaurants.push(restaurant);
+                });
+                $scope.total = response.totalPages;
+                $scope.$broadcast('scroll.infiniteScrollComplete');
+            }).error(function (err) {
+                $scope.$broadcast('scroll.infiniteScrollComplete');
+                console.log(err);
+            });
+        };
+
+        $scope.getRestaurants();
     });
-  };
-  
-  // Carrega a primeira página dos restauradas da API de carregamento
-  $scope.getRestaurants();
-});
