fa45918ff8999de4a0d314ef7334b90ffea4b316
Sant'Clear Ali Costa <santclear@gmail.com>
2016-04-27 19:29:12 -0300

    Aula 1 - Criando um gerenciador de finanças

--- wsp_youtube_criando_um_gerenciador_de_financas_com_ionic2/AnsoFinances/app/app.js
+++ wsp_youtube_criando_um_gerenciador_de_financas_com_ionic2/AnsoFinances/app/app.js
@@ -2,23 +2,30 @@ import {App, Platform} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HomePage} from './pages/home/home';
 
-
+// @App indica o que a classe é ou o que a classe faz
 @App({
-  template: '<ion-nav [root]="rootPage"></ion-nav>',
+  // 1. adiciona variável index a raiz
+  template: '<ion-nav [root]="index"></ion-nav>',
   config: {} // http://ionicframework.com/docs/v2/api/config/Config/
 })
+
+// definição de classe
 export class MyApp {
+  // passa o parâmetro para o construtor
   static get parameters() {
     return [[Platform]];
   }
 
+  // construtor de classe
   constructor(platform) {
-    this.rootPage = HomePage;
+    // 2. variável index recebe a página definida no "import {HomePage}"
+    this.index = HomePage;
 
+    // método
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
+      StatusBar.styleDefault(); // aplica o estilo default ao statu bar
     });
   }
 }
--- wsp_youtube_criando_um_gerenciador_de_financas_com_ionic2/AnsoFinances/app/pages/home/home.js
+++ wsp_youtube_criando_um_gerenciador_de_financas_com_ionic2/AnsoFinances/app/pages/home/home.js
@@ -1,11 +1,16 @@
 import {Page} from 'ionic-angular';
 
-
+// @Page definindo a função da classe, nesse caso o que ela é
+// ou seja uma página
 @Page({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
   constructor() {
-
+    this.nome = "AnsoDev"
+  }
+  
+  geNome() {
+    return "Retornado pelo método: "+ this.nome;
   }
 }
